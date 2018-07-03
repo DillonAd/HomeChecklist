5891b5bf11378c5b4ac5ba0016aaa6ee22935eca
Dillon <dillon-a@hotmail.com>
2018-07-03 11:05:43 -0500

    adding logging to dbcontext

--- src/api/HomeChecklist.Persistence/HomeChecklistDbContext.cs
+++ src/api/HomeChecklist.Persistence/HomeChecklistDbContext.cs
@@ -1,11 +1,16 @@
 using HomeChecklist.Persistence.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using System.Collections.Generic;
 
 namespace HomeChecklist.Persistence
 {
     public class HomeChecklistDbContext : DbContext
     {
+        private static ILoggerFactory loggerFactory = 
+            new LoggerFactory(new [] { new ConsoleLoggerProvider((_, __) => true, true) });
+
         public virtual DbSet<Home> Homes { get; set; }
         public virtual DbSet<Resident> Residents { get; set; }
         public virtual DbSet<Room> Rooms { get; set; }
@@ -18,6 +23,9 @@ namespace HomeChecklist.Persistence
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseSqlServer("")
+                .UseLoggerFactory(loggerFactory);
+                
             base.OnConfiguring(optionsBuilder);
         }
     }
