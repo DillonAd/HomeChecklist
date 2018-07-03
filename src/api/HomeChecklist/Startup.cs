fe7ca39b9646609c0b860b73ab9889f8a4a82f7a
Dillon <dillon-a@hotmail.com>
2018-07-02 22:16:32 -0500

    minor swagger config

--- src/api/HomeChecklist/Startup.cs
+++ src/api/HomeChecklist/Startup.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HomeChecklist.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HomeChecklist
 {
@@ -39,6 +42,8 @@ namespace HomeChecklist
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
 
             app.UseHttpsRedirection();
             app.UseMvc();
