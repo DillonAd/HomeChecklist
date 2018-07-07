c50dc232f7d4a004dc46f0a4cd799763533952ae
Dillon <dillon-a@hotmail.com>
2018-07-06 21:07:43 -0500

    adding room controller

---
+++ src/api/HomeChecklist/Controllers/RoomController.cs
@@ -0,0 +1,17 @@
+﻿using HomeChecklist.Repository;
+using HomeChecklist.Repository.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeChecklist.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase<Room>
+    {
+        public HomeController(IRepo<Room> roomRepo) : base(roomRepo) { }
+    }
+}
