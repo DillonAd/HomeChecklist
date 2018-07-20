31d35374389ef1dbef59eb9010f273b2b8c4b905
Dillon <dillon-a@hotmail.com>
2018-07-19 21:06:45 -0500

    adding HtmlModule to app.module

--- src/web/HomeChecklist/src/app/app.module.ts
+++ src/web/HomeChecklist/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http'
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { TaskComponent } from './task/task.component';
-import { HomeListComponent } from './home-list/home-list.component';
 import { HomeComponent } from './home/home.component';
+import { HomeListComponent } from './home-list/home-list.component';
+import { HomeService } from './services/home.service';
+import { ResidentService } from './services/resident.service';
+import { RoomService } from './services/room.service';
+import { TaskComponent } from './task/task.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskService } from './services/task.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +23,16 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HomeService,
+    ResidentService,
+    RoomService,
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
