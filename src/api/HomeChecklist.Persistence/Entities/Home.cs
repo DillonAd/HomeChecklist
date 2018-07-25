f79d9071856feee93c66f6995917f759d29deb63
Dillon <dillon-a@hotmail.com>
2018-07-25 13:44:23 -0500

    removing FK relations for now

--- src/api/HomeChecklist.Persistence/Entities/Home.cs
+++ src/api/HomeChecklist.Persistence/Entities/Home.cs
@@ -6,13 +6,7 @@ namespace HomeChecklist.Persistence.Entities
     public class Home : Entity
     {
         public string Name { get; set; }
-        public virtual ICollection<Resident> Residents { get; set; }
-        public virtual ICollection<Room> Rooms { get; set; }
 
-        public Home()
-        {
-            Residents = new List<Resident>();
-            Rooms = new List<Room>();
-        }
+        public Home() { }
     }
 }
--- src/api/HomeChecklist.Persistence/Entities/Room.cs
+++ src/api/HomeChecklist.Persistence/Entities/Room.cs
@@ -6,11 +6,5 @@ namespace HomeChecklist.Persistence.Entities
     public class Room : Entity
     {
         public string Name { get; set; }
-        public List<Task> Tasks { get; set; }
-
-        public Room()
-        {
-            Tasks = new List<Task>();
-        }
     }
 }
