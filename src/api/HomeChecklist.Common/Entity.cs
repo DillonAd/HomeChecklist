4f6596dc7aaf2aa161abfd2fce35c0edca97bf72
Dillon <dillon-a@hotmail.com>
2018-07-25 13:42:47 -0500

    designating key and identity for entities

--- src/api/HomeChecklist.Common/Entity.cs
+++ src/api/HomeChecklist.Common/Entity.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace HomeChecklist.Common
 {
     public abstract class Entity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
