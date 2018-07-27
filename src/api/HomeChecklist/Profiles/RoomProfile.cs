b9600c0072669b5c9e79cd491e5646910ba6ebb9
Dillon <dillon-a@hotmail.com>
2018-07-27 15:37:15 -0500

    adding room profile

---
+++ src/api/HomeChecklist/Profiles/RoomProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using HomeChecklist.DTO;
+using HomeChecklist.Persistence.Entities;
+
+namespace HomeChecklist.Profiles
+{
+    public class RoomProfile : Profile
+    {
+        public RoomProfile()
+        {
+            CreateMap<RoomDTO, Room>()
+                .ForMember(r => r.Id, m => m.MapFrom(r => r.Id))
+                .ForMember(r => r.Name, m => m.MapFrom(r => r.Name))
+                .ForMember(r => r.HomeId, m => m.Ignore());
+        }
+    }
+}
