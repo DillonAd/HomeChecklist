dc23da9535c6cb31e108c0747706916502bbe920
Dillon <dillon-a@hotmail.com>
2018-07-26 20:00:51 -0500

    adding profile and adjusting DTO for task

--- src/api/HomeChecklist/DTO/TaskDTO.cs
+++ src/api/HomeChecklist/DTO/TaskDTO.cs
@@ -5,6 +5,7 @@ namespace HomeChecklist.DTO
 {
     public class TaskDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public DateTime LastDone { get; set; }
         public TimeSpan Interval { get; set; }
---
+++ src/api/HomeChecklist/Profiles/TaskProfile.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using HomeChecklist.DTO;
+using HomeChecklist.Persistence.Entities;
+
+namespace HomeChecklist.Profiles
+{
+    public class TaskProfile : Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<TaskDTO, Task>()
+                .ForMember(t => t.Id, m => m.MapFrom(t => t.Id))
+                .ForMember(t => t.Name, m => m.MapFrom(t => t.Name))
+                .ForMember(t => t.Interval, m => m.MapFrom(t => t.Interval))
+                .ForMember(t => t.LastDone, m => m.MapFrom(t => t.LastDone))
+                .ForMember(t => t.HomeId, m => m.Ignore())
+                .ForMember(t => t.ResidentId, m => m.Ignore());
+        }
+    }
+}
