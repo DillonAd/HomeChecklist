02ceb4dbcc48b8611981cbe60cb4b63392bd3927
Dillon <dillon-a@hotmail.com>
2018-08-04 09:36:10 -0500

    allowing Id to be nullable for inserts. probably need some mitigation for updates

--- src/api/HomeChecklist.Common/Entity.cs
+++ src/api/HomeChecklist.Common/Entity.cs
@@ -7,6 +7,6 @@ namespace HomeChecklist.Common
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public int? Id { get; set; }
     }
 }
--- src/api/HomeChecklist/DTO/HomeDTO.cs
+++ src/api/HomeChecklist/DTO/HomeDTO.cs
@@ -5,7 +5,7 @@ namespace HomeChecklist.DTO
 {
     public class HomeDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
     }
 }
