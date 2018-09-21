4c6146f8d13f01f30079a19cfae3c704c5e2f2a2
Dillon Adams <dillon-a@hotmail.com>
2018-09-20 20:41:08 -0500

    Adding events for create and update

--- src/web/HomeChecklist/src/app/room/room.component.ts
+++ src/web/HomeChecklist/src/app/room/room.component.ts
@@ -1,5 +1,5 @@
 import { Room } from './../interfaces/room';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-room',
@@ -10,10 +10,19 @@ export class RoomComponent implements OnInit {
 
   @Input() homeId: number;
   @Input() room: Room;
+  @Output() roomCreated: EventEmitter<Room> = new EventEmitter();
+  @Output() roomChanged: EventEmitter<Room> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  createRoom() {
+    this.roomCreated.emit(this.room);
+  }
+
+  updateRoom() {
+    this.roomChanged.emit(this.room);
+  }
 }
