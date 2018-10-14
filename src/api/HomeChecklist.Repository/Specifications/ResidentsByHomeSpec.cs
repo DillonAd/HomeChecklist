c9614459329223d652a7b78dc74bb48d45d3dc47
Dillon Adams <dillon-a@hotmail.com>
2018-10-14 09:00:46 -0500

    Adding ResidentsByHome Specification

---
+++ src/api/HomeChecklist.Repository/Specifications/ResidentsByHomeSpec.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using HomeChecklist.Persistence.Entities;
+
+namespace HomeChecklist.Repository.Specifications
+{
+    internal class ResidentsByHomeSpec : Specification<Resident>
+    {
+        private readonly int _homeId;
+
+        public ResidentsByHomeSpec(int homeId)
+        {
+            _homeId = homeId;
+        }
+
+        public override Expression<Func<Resident, bool>> Expression =>
+            t => t.HomeId == _homeId;
+    }
+}
