932b4d1233f3abf9beb0d72b82cfbf7b5dfe3f39
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-23 13:10:27 -0500

    Specifying HTTP methods in the Resident controller

--- src/api/HomeChecklist/Controllers/ResidentController.cs
+++ src/api/HomeChecklist/Controllers/ResidentController.cs
@@ -20,24 +20,28 @@ namespace HomeChecklist.Controllers
             _mapper = mapper;
         }
 
+        [HttpPost]
         public async Task CreateResident(ResidentDTO dto)
         {
             var resident = _mapper.Map<Resident>(dto);
             await _repo.CreateResident(resident);
         }
 
+        [HttpGet]
         public async Task<ResidentDTO[]> GetResidents(int homeId)
         {
             var residents = await _repo.GetResidents(homeId);
             return _mapper.Map<ResidentDTO[]>(residents);
         }
 
+        [HttpGet("{residentId}")]
         public async Task<ResidentDTO> GetResident(int residentId)
         {
             var resident = await _repo.GetResident(residentId);
             return _mapper.Map<ResidentDTO>(resident);
         }
 
+        [HttpPut]
         public async Task UpdateResident(ResidentDTO dto)
         {
             var resident = _mapper.Map<Resident>(dto);
