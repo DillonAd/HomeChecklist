e41c5e46988a127561cefa93143eae01de4b814b
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-24 19:47:03 -0500

    Moving to expression body members where appropriate in the nested Repo

--- src/api/HomeChecklist.Repository/Repo.cs
+++ src/api/HomeChecklist.Repository/Repo.cs
@@ -19,26 +19,18 @@ namespace HomeChecklist.Repository
             _dbSet = _context.Set<T>();
         }
 
-        public async Task<T> GetSingle(Specification<T> spec)
-        {
-            return await _dbSet.FirstOrDefaultAsync(spec.Expression);
-        }
+        public async Task<T> GetSingle(Specification<T> spec) =>
+            await _dbSet.FirstOrDefaultAsync(spec.Expression);
 
-        public async Task<IEnumerable<T>> Get(Specification<T> spec)
-        {
-            return await _dbSet.Where(spec.Expression).ToListAsync();
-        }
+        public async Task<IEnumerable<T>> Get(Specification<T> spec) =>
+            await _dbSet.Where(spec.Expression).ToListAsync();
 
-        public async Task Insert(T t)
-        {
+        public async Task Insert(T t) =>
             await _dbSet.AddAsync(t);
-        }
 
-        public async Task Update(T t)
-        {
+        public async Task Update(T t) =>
             await Task.FromResult(_dbSet.Update(t));
-        }
-
+        
         public async Task Delete(Specification<T> spec)
         {
             var targets = await Get(spec);
